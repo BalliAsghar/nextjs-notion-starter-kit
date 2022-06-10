5ae9de2b7ee89ec377ed4ebdee8547ef367870a4
Balli Asghar <ballihacker@outlook.com>
2022-06-10 19:12:24 +0100

    init

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '0b532d118beb4bff8ec3ea10cf133fd6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Muhammad Asghar - Portfolio',
+  domain:
+    'https://attractive-peony-bd0.notion.site/Next-js-Notion-Starter-Kit-Template-0b532d118beb4bff8ec3ea10cf133fd6',
+  author: 'Muhammad Asghar',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Muhammad Asghar - Portfolio',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  github: 'BalliAsghar',
+  linkedin: 'moasghar',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
